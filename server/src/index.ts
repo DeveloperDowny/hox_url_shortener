f09b42f460b5e2bd2cd3b8f10b78c06e63e8f79c
DeveloperDowny <vedantpanchal12345@gmail.com>
2024-08-08 13:35:33 +0530

    added request and response validator

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,8 @@ import shortLinkRouter from "./routes/short_links";
 
 import cors from "cors";
 
+import * as OpenApiValidator from "express-openapi-validator";
+
 dotenv.config();
 
 const app: Express = express();
@@ -14,6 +16,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: "./public/openapi.json",
+    validateRequests: true,
+    validateResponses: true,
+  })
+);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
